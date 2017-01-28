3c96a5042dd72ffd0d610b889c386006048ea5c8
Женя Филиппович <zhenya.filippovich@gmail.com>
2017-01-28 16:28:41 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,24 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min = 0;
+    	this.max = 0;
+    	this.value = 0;
+    	this.result = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
+    	this.value = max;
     }
 
     guess() {
-
+    	this.result = Math.ceil(this.min + (this.max - this.min)/2);
+    	return this.result;
     }
 
     lower() {
-
+    	this.max = this.result;
     }
 
     greater() {
-
-    }
-
-    set value() {
-    	return 43;
+    	this.min = this.result;
     }
 }
 
