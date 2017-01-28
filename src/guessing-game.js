986d2ab0fbf6b9fc0de3be9a2879514f21e8598a
filipochka97 <zhenya.filippovich@gmail.com>
2017-01-28 16:22:20 +0300

    Revert "Update guessing-game.js"
    This reverts commit c0db2411da55c3c2bb7becbdc9b72196cbcf6680.

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,28 +1,20 @@
 class GuessingGame {
-    constructor() {
-    	this.min = 0;
-    	this.max = 0;
-    	this.value = 0;
-    	this.result = 0;
-    }
+    constructor() {}
 
     setRange(min, max) {
-    	this.min = min;
-    	this.max = max;
-    	this.value = max;
+
     }
 
     guess() {
-    	this.result = Math.ceil(this.min + (this.max - this.min)/2);
-    	return this.result;
+
     }
 
     lower() {
-    	this.max = this.result;
+
     }
 
     greater() {
-    	this.min = this.result;
+
     }
 
     set value() {
